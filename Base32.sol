7af8cea92acfbf43b7a65d5529d17a9d862165f1
breadNbutter42 <97899677+breadNbutter42@users.noreply.github.com>
2023-11-21 20:27:29 -0500

    Update Base32.sol added disclaimer

--- Base32.sol
+++ Base32.sol
@@ -1,4 +1,5 @@
-//written largely by chatgpt may have errors not fully tested
+//Don't use, written largely by chatgpt seems to be full of errors and wrong numbers used and comments are wrong
+
 //maybe look at https://etherscan.io/address/0x7dd7aa4b560692c882d982312c0b53b24c51523a#code for base32 stuff for cidv1 encoding
 
 // SPDX-License-Identifier: MIT
